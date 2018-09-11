4921f2f36ac6df58ab55fa423d7a1e0332ee1762
self-unit <self_unit@hotmail.com>
2018-09-11 18:10:25 +0100

    ext partially complete

--- src/app.js
+++ src/app.js
@@ -3,20 +3,20 @@ const ResultView = require('./views/result_view.js');
 const InstrumentFamilies = require('./models/instrument_families.js');
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+  // console.log('JavaScript Loaded');
 
   const selectItem = document.querySelector('select#instrument-families');
   const instrumentMenu = new SelectView(selectItem);
   instrumentMenu.bindEvents();
-  console.log(instrumentMenu);
+  // console.log(instrumentMenu);
 
   const familyDiv = document.querySelector('div#family-info');
   const showFamilyDetails = new ResultView(familyDiv);
   showFamilyDetails.bindEvents();
-  console.log(showFamilyDetails);
+  // console.log(showFamilyDetails);
 
   const instrumentDataArray = new InstrumentFamilies();
   instrumentDataArray.bindEvents();
-  console.log(instrumentDataArray);
+  // console.log(instrumentDataArray);
 
 });
--- src/views/result_view.js
+++ src/views/result_view.js
@@ -12,16 +12,26 @@ ResultView.prototype.bindEvents = function () {
 };
 
 ResultView.prototype.render = function (result) {
-  const title = document.createElement('h2');
-  title.textContent = `${result.name}`;
-  this.container.appendChild(title);
+  const familyTitle = document.createElement('h2');
+  familyTitle.textContent = `${result.name}`;
+  this.container.appendChild(familyTitle);
 
   const infoParagraph = document.createElement('p');
   infoParagraph.setAttribute('style', 'white-space: pre-line;');
   infoParagraph.textContent = `${result.description}`;
   this.container.appendChild(infoParagraph);
 
-  
+  const listTitle = document.createElement('h3');
+  listTitle.textContent = 'Instruments include:';
+  this.container.appendChild(listTitle);
+
+  const instrumentList = document.createElement('ul');
+  this.container.appendChild(instrumentList);
+  result.instruments.forEach(function(instrument) {
+    const instrumentItem = document.createElement('li');
+    instrumentItem.textContent = `${instrument}`;
+    instrumentList.appendChild(instrumentItem);
+  });
 };
 
 module.exports = ResultView;
